598b8abf9a48ab44c9fd1fd84c50ff319a8d1eee
Soroush <ghaderi.soroush1995@gmail.com>
2021-05-27 16:32:13 +0430

    add helper for cleaning

---
+++ helper_cleaning.py
@@ -0,0 +1,36 @@
+def standard_column_names(dataframe):
+    """ Edit column names, remove space and replace with underline,
+        then set column names lowercase
+    Parameters
+    ----------
+    dataframe : pandas DataFrame
+        DataFrame with non-standardard column names
+
+    Returns
+    -------
+    dataframe : Pandas DataFrame
+        Edited Dataframe with standard column names
+    
+    """
+    return dataframe.columns.str.replace(" ", "_").str.lower()
+
+
+def standard_features_string(dataframe):
+    for column in dataframe.columns:
+        if dataframe[column].dtype == "object":
+            dataframe[column] = dataframe[column].str.replace(" ", "_").str.lower()
+        else:
+            pass
+    return dataframe
+
+
+def get_feature_names_mixture_column_transformer(mixture_transformer, cat_features, numerical_name, categorical_name):
+    full_transformer = mixture_transformer.transformers_
+    all_features = []
+    for name, transformer, features in full_transformer:
+        if name == numerical_name:
+            all_features.append(features)
+        elif name == categorical_name:
+            all_features.append(transformer.named_steps["One-Hot"].get_feature_names(cat_features).tolist())
+    flat_features = [item for sublist in all_features for item in sublist]
+    return flat_features
