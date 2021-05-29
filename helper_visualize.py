86d6ebd9a5d3aa971c0f426a72662e6e1075c8b9
Soroush <ghaderi.soroush1995@gmail.com>
2021-05-29 10:13:59 +0430

    edit helper visualize

--- helper_visualize.py
+++ helper_visualize.py
@@ -1,8 +1,18 @@
-def height_notation(plot_name , coordinator , dataframe , color="white", percent = False):
+def height_notation(plot_name , coordinator , dataframe , color="white", percent=False, height_type=int):
+    """
+    Args:
+        plot_name ([matplotlib plot]): 
+            name of plot
+        coordinator ([integer]): [description]
+        dataframe ([type]): [description]
+        color (str, optional): [description]. Defaults to "white".
+        percent (bool, optional): [description]. Defaults to False.
+        height_type ([type], optional): [description]. Defaults to int.
+    """
     total = len(dataframe)
     for p in plot_name.patches:
         height = p.get_height().round(2)
-        text = str(float(height))
+        text = str(height_type(height))
         if percent == True:
             plot_name.text(p.get_x() + p.get_width() / 2 , height + coordinator , text + ' | ' + str(height * 100 // total) + '%' , ha = 'center', color="white")
         else:
