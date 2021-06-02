644ecb586875cabca7ec93fcf19adf34ead7e5d3
Soroush <ghaderi.soroush1995@gmail.com>
2021-06-02 11:06:15 +0430

    edit data files

---
+++ Airline.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Author:  Soroush Ghaderi <ghaderi.soroush1995@gmail.com>
+Purpose: Airline Satisfaction Network
+"""
+
+import argparse
+
+
+
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n',
+                        '--name',
+                        default='World',
+                        help='Name to greet')
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
